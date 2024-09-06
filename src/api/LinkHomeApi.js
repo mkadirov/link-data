7516aaf935a2d459960c72eedea02cc19dc82523
Kadirov1993 <mkodirjonov@mail.ru>
2024-09-06 13:53:50 +0200

    update0609

--- src/api/LinkHomeApi.js
+++ src/api/LinkHomeApi.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const baseURL = "http://104.248.155.201/api/springboot/getHomePrice"
+const baseURL = "https://linkhomeapi.eu/springboot/api/getHomePrice"
+// const baseURL = "https://linkhomeapi.eu/springboot/api/test"
+const baseURLDjango = "https://linkhomeapi.eu/django/home/getPrice/"
 
 
 
@@ -9,7 +11,33 @@ const getHomePrice = async(formData) => {
     try {
         console.log(formData);
         
-        const res = await axios.post(baseURL, formData);
+        const res = await axios.post(baseURL, formData, {
+            headers: {
+              'Content-Type': 'application/json'
+            },
+        });
+        
+        if(res.status == 200) {
+            return {success: true, data: res.data}
+        }else {
+            return {success: false}
+        }
+    } catch (error) {
+        return {success: false}
+    }
+}
+
+const getHomePriceDjango = async(formData) => {
+
+    try {
+        console.log(formData);
+        
+        // const res = await axios.get(baseURL, formData);
+        const res = await axios.options(baseURL , formData, {
+            headers: {
+              'Content-Type': 'application/json'
+            },
+        });
         if(res.status == 200) {
             return {success: true, data: res.data}
         }else {
